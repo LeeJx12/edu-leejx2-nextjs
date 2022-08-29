bbad3a683ea6dd370b47747281c0730ad4738f5d
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-08-29 23:13:45 +0900

    Feat: audioplayer, audiomixer 진입점 작성
    audioplayer, audiomixer 진입점 작성

---
+++ pages/typescript/audioplayer/index.tsx
@@ -0,0 +1,14 @@
+import styles from '../../../styles/Typescript.module.css'
+
+export default function AudioPlayer(): JSX.Element {
+    return (
+        <>
+            <div className="container-fluid p-0">
+                <div className="">
+                </div>
+                <div className="">
+                </div>
+            </div>
+        </>
+    )
+}
--- pages/typescript/index.tsx
+++ pages/typescript/index.tsx
@@ -1,15 +1,50 @@
 import Link from "node_modules/next/link";
+import styles from '../../styles/Typescript.module.css'
 
-
-export default function tsRender(): JSX.Element {
+export default function Typescript(): JSX.Element {
     return (
         <>
-            <h1>Typescript Practice Page!</h1>
-            <h2>
-                <Link href="/">
-                    <a>홈으로 돌아가기</a>
-                </Link>
-            </h2>
+            <div className="container text-center">
+                <div className="row my-5">
+                    <div className="col">
+                        <h1>Typescript Practice</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className={'card' + ' ' + styles.card}>
+                            <div className="card-body">
+                                <h5 className="card-title">홈으로 돌아가기</h5>
+                                <Link href="/">
+                                    <p className="card-text cur_p">/</p>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <div className={'card' + ' ' + styles.card}>
+                            <div className="card-body">
+                                <h5 className="card-title">Audio Player</h5>
+                                <Link href="/typescript/audioplayer">
+                                    <p className="card-text cur_p">/typescript/audioplayer</p>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className={'card' + ' ' + styles.card}>
+                            <div className="card-body">
+                                <h5 className="card-title">Audio Mixer</h5>
+                                <Link href="/typescript/audiomixer">
+                                    <p className="card-text cur_p">/typescript/audiomixer</p>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
