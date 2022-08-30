36005d02e5fcedbc322e16855930577c5d684f48
LeeJx2 <46637231+LeeJx12@users.noreply.github.com>
2022-08-30 11:30:29 +0900

    Feat: recoil 추가
    recoil 추가

--- pages/typescript/audioplayer/components.tsx
+++ pages/typescript/audioplayer/components.tsx
@@ -1,9 +1,15 @@
 import React, { Dispatch, SetStateAction } from 'react';
 import { Track } from './types';
+import { RecoilState, useRecoilState, useRecoilValue } from 'node_modules/recoil/index';
 
 import styles from '../../../styles/Typescript.module.css'
 
-export function AudioList(props: { _setTrack: React.Dispatch<SetStateAction<Track>> }): JSX.Element {
+export function AudioList(props: { _trackState: RecoilState<Track> }): JSX.Element {
+    //TODO: getTracks 추가할것
+
+    const { _trackState } = props;
+    const [ setTrack ] = useRecoilState(_trackState);
+
     return (
         <>
 
@@ -11,7 +17,10 @@ export function AudioList(props: { _setTrack: React.Dispatch<SetStateAction<Trac
     );
 }
 
-export function AudioPlayer(props: { _track: any }): JSX.Element {
+export function AudioPlayer(props: { _trackState: RecoilState<Track> }): JSX.Element {
+    const { _trackState } = props;
+    const track = useRecoilValue(_trackState);
+
     return (
         <>
             <audio id="player"></audio>
--- pages/typescript/audioplayer/index.tsx
+++ pages/typescript/audioplayer/index.tsx
@@ -1,18 +1,23 @@
-import { useState } from 'react'
 import { AudioList, AudioPlayer } from './components'
 import { Track } from './types';
+import { RecoilRoot, atom } from 'node_modules/recoil/index';
 
 import styles from '../../../styles/Typescript.module.css'
 
 export default function Index(): JSX.Element {
-    const [ track, setTrack ] = useState(undefined as Track);
+    const trackState = atom({
+        key: 'selectedTrack',
+        default: undefined as Track
+    });
 
     return (
         <>
-            <div className="container-fluid p-0">
-                <AudioList _setTrack={setTrack}/>
-                <AudioPlayer _track={track}/>
-            </div>
+            <RecoilRoot>
+                <div className="container-fluid p-0">
+                    <AudioList _trackState={trackState}/>
+                    <AudioPlayer _trackState={trackState}/>
+                </div>
+            </RecoilRoot>
         </>
     )
 }
